6e8e7237111004a0e83cde00aa58a6f23d64aa14
emildev666 <emilg.242@gmail.com>
2022-07-22 20:41:39 +0400

    init3

--- cypress/e2e/test/boilerplate.cy.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-describe('It should ', function () {
-    it('should visit the website', function () {
-        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
-    });
-});
--- cypress/e2e/test/wt-test.cy.js
+++
@@ -1,9 +0,0 @@
-// describe('open website', function () {
-//
-//     it('should ', function () {
-//         cy.visit('https://pre.wetravel.to/');
-//         cy.wait(3000);
-//         cy.get('a').contains("SIGN IN").click();
-//         cy.get('input#login_email').type('emil@mailinator.com');
-//     });
-// });
